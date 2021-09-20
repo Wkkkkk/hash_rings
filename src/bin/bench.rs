26647bf282095775c34189f968ae1e0087a71590
kunwu <wk707060335@gmail.com>
2021-09-20 20:03:01 +0200

    add result output

--- src/bin/bench.rs
+++ src/bin/bench.rs
@@ -1,6 +1,8 @@
 use hash_rings::consistent;
-use hash_rings::generator;
+use hash_rings::generator::{Generator, KeyDistribution};
 
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 use rand::Rng;
 use std::collections::HashMap;
 use std::time::{Duration, Instant};
@@ -9,15 +11,16 @@ const REPLICAS: u64 = 100;
 const ITEMS: u64 = 100_000;
 const NODES: u64 = 10;
 
-fn print_node_statistic(id: u64, expected: f64, actual: f64) {
+fn print_node_statistic(id: u64, expected: f64, actual: f64) -> f64 {
     let error = (expected - actual) / expected;
     println!(
         "{:020} - Expected: {:.6} | Actual: {:.6} | Error: {:9.6}",
         id, expected, actual, error,
     );
+    error
 }
 
-fn print_bench_statistic(duration: Duration) {
+fn print_bench_statistic(duration: Duration) -> f64 {
     let total_time = duration.as_secs() as f64 * 1e9 + f64::from(duration.subsec_nanos());
     let ns_per_op = total_time / ITEMS as f64;
     let ops_per_ns = 1e9 / ns_per_op;
@@ -26,49 +29,77 @@ fn print_bench_statistic(duration: Duration) {
     println!("Milliseconds per operation: {:>10.3} ns", ns_per_op);
     println!("Operations per second:      {:>10.3} op/ms", ops_per_ns);
     println!();
+    ops_per_ns
 }
 
-fn bench_consistent() {
+fn write_result()
+
+fn bench_consistent(num_nodes: u64, num_replicas: u64, num_items: u64, dis: KeyDistribution) {
     println!(
         "\nBenching consistent hashing ({} nodes, {} replicas, {} items)",
-        NODES, REPLICAS, ITEMS,
+        num_nodes, num_replicas, num_items,
     );
     let mut rng = rand::thread_rng();
 
     let mut occ_map = HashMap::new();
     let mut nodes = Vec::new();
     let mut ring = consistent::Ring::new();
-    let total_replicas = REPLICAS * NODES;
+    let total_replicas = num_replicas * num_nodes;
 
-    for _ in 0..NODES {
+    for _ in 0..num_nodes {
         let id = rng.gen::<u64>();
         occ_map.insert(id, 0f64);
         nodes.push(id);
     }
 
     for node in &nodes {
-        ring.insert_node(node, REPLICAS as usize);
+        ring.insert_node(node, num_replicas as usize);
     }
 
-    let mut g = generator::Generator::new();
-    let workload: Vec<u64> = g.next_n(ITEMS);
+    let mut key_generator = Generator::new(dis);
+    let workload: Vec<u64> = key_generator.next_n(num_items);
 
     let start = Instant::now();
-    for key in workload {
-        let id = ring.get_node(&key);
+    for item in workload {
+        let id = ring.get_node(&item);
         *occ_map.get_mut(id).unwrap() += 1.0;
     }
 
-    for node in &nodes {
-        print_node_statistic(
-            *node,
-            REPLICAS as f64 / total_replicas as f64,
-            occ_map[&node] / ITEMS as f64,
-        );
-    }
-    print_bench_statistic(start.elapsed());
+    let variances = nodes.iter()
+        .map(|node| {
+            print_node_statistic(
+                *node,
+                num_replicas as f64 / total_replicas as f64,
+                occ_map[&node] / num_items as f64,
+            )
+        })
+        .map(|v| {
+            v.to_string()
+        })
+        .collect::<Vec<_>>()
+        .join("\t");
+
+    let throughput = print_bench_statistic(start.elapsed());
+
+    let output_str = format!("{}\t{}\t{:}\t{}\t{}\n", num_items, num_nodes, dis, throughput, variances);
+    let mut f = OpenOptions::new()
+        .append(true)
+        .create(true) // Optionally create the file if it doesn't already exist
+        .open("./src/scripts/consistent_hashing.csv")
+        .expect("Unable to open file");
+    f.write_all(output_str.as_bytes()).expect("Unable to write data");
 }
 
 fn main() {
-    bench_consistent();
+    let nodes_list = 3..=20;
+    let items_list = (1000..=10_000).step_by(1000);
+    // let replica_list = range(10, 100, 10);
+
+    for nodes in nodes_list {
+        for items in items_list.clone() {
+            bench_consistent(nodes, REPLICAS, items, KeyDistribution::uniform_distribution());
+            bench_consistent(nodes, REPLICAS, items, KeyDistribution::normal_distribution());
+            bench_consistent(nodes, REPLICAS, items, KeyDistribution::lognormal_distribution());
+        }
+    }
 }
--- src/generator.rs
+++ src/generator.rs
@@ -1,30 +1,62 @@
 // import commonly used items from the prelude:
+use std::fmt;
 use rand::Rng;
 use rand::distributions::{Uniform, Distribution};
 use rand_distr::{Normal, LogNormal};
 
-const MAX_KEY: f64 = 10.0;
+const MAX_KEY: f64 = 100.0;
 const MEDIUM_KEY: f64 = MAX_KEY / 2.0;
+
+#[derive(Copy, Clone)]
+pub enum KeyDistribution {
+    UNIFORM(rand::distributions::Uniform<f64>),
+    NORMAL(rand_distr::Normal<f64>),
+    LOGNORMAL(rand_distr::LogNormal<f64>),
+}
+
+impl fmt::Display for KeyDistribution {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            KeyDistribution::UNIFORM(_) => write!(f, "uniform"),
+            KeyDistribution::NORMAL(_) => write!(f, "normal"),
+            KeyDistribution::LOGNORMAL(_) => write!(f, "lognormal"),
+        }
+    }
+}
+
+impl KeyDistribution {
+    pub fn uniform_distribution() -> KeyDistribution {
+        KeyDistribution::UNIFORM(Uniform::new(0.0, MAX_KEY))
+    }
+
+    pub fn normal_distribution() -> KeyDistribution {
+        KeyDistribution::NORMAL(Normal::new(5.0, 1.0).unwrap())
+    }
+
+    pub fn lognormal_distribution() -> KeyDistribution {
+        KeyDistribution::LOGNORMAL(LogNormal::new(5.0, 1.0).unwrap())
+    }
+}
 
 pub struct Generator {
     rand: rand::rngs::ThreadRng,
-    uniform: rand::distributions::Uniform<f64>,
-    normal: rand_distr::Normal<f64>,
-    lognormal: rand_distr::LogNormal<f64>,
+    dis: KeyDistribution,
 }
 
 impl Generator {
-    pub fn new() -> Self {
+    pub fn new(dis: KeyDistribution) -> Self {
         Self {
             rand: rand::thread_rng(),
-            uniform: Uniform::new(0.0, MAX_KEY),
-            normal: Normal::new(MEDIUM_KEY, 1.0).unwrap(),
-            lognormal: LogNormal::new(MEDIUM_KEY, 1.0).unwrap(),
+            dis,
         }
     }
 
     pub fn next(&mut self) -> u64 {
-        self.rand.sample(self.normal).floor() as u64
+        match self.dis {
+            KeyDistribution::UNIFORM(x) => self.rand.sample(x).floor() as u64,
+            KeyDistribution::NORMAL(x) => self.rand.sample(x).floor() as u64,
+            KeyDistribution::LOGNORMAL(x) => self.rand.sample(x).floor() as u64
+        }
     }
 
     pub fn next_n(&mut self, n: u64) -> Vec<u64> {
